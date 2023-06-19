86371037bb4f3026d8b8376b4bbab833fdd01065
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-06-19 16:30:50 -0500

    Add pseudocode to script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Create a grid of square divs
+
+// Apply flexbox to div to make square grid
+
+// Add hovering effect that changes color of boxes
+
+// Add button with popup
+// Ask how large user wants the grid (max 100)
+// Replace current grid with new grid (space taken up should remain the same)
+
+// Create color options
+// Randomized colors
+// User color choice
+// Progressively darken (10% darker per mouse over)
+// Eraser (changes back to white)
+
+// Create clear button
