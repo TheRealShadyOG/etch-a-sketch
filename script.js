632874d6650e6aa5de044521e33f35a7b9de0f5f
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-06-21 19:14:03 -0500

    Add button for reseting grid size

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ for (i = 1; i <= gridSize; i++) {
 grid.style.cssText = "height: 500px; width: 500px; display: flex; flex-wrap: wrap;"
                    + "border: solid 2px black;";
 
-document.body.style.cssText = "display: flex; justify-content: center;";
+document.body.style.cssText = "display: flex; flex-direction: column; align-items: center;";
 
 // Add hovering effect that changes color of boxes
 let color = "black"
@@ -28,6 +28,15 @@ function changeColor(ele) {
 }
 
 // Add button with popup
+const options = document.createElement("div");
+options.setAttribute("id", "options");
+document.body.insertBefore(options, grid);
+
+const buttonNewSize = document.createElement("button");
+buttonNewSize.setAttribute("id", "button");
+buttonNewSize.textContent = "Resize Grid";
+options.appendChild(buttonNewSize);
+
 // Ask how large user wants the grid (max 100)
 // Replace current grid with new grid (space taken up should remain the same)
 
